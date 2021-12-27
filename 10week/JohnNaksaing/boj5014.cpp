97df12eef61a33f0f32d80beab59494de08e120e
johnnaksaing <johnnaksaing@daum.net>
2021-12-27 23:38:58 +0900

    add boj5014.cpp
    add code + markdown
    fix makrdown link

---
+++ 10week/JohnNaksaing/boj5014.cpp
@@ -0,0 +1,54 @@
+#pragma once
+#include <iostream>
+#include <queue>
+#include <vector>
+
+struct elevator_info 
+{
+	int floor;
+	int howmany_move;
+
+	static std::vector<bool> bVisited;
+};
+std::vector<bool> elevator_info::bVisited;
+
+template<typename Ty>
+using search_ds_type = std::queue<Ty>;
+using node = elevator_info;
+
+int main()
+{	
+	int F, S, G, U, D;
+	std::cin >> F >> S >> G >> U >> D;
+
+	elevator_info::bVisited = std::vector<bool>(F+1,false);
+	search_ds_type<node> elevator;
+
+	elevator_info::bVisited[S] = true, elevator.push({ S,0 });
+	
+	int answer_howmany_move = -1;
+	while (!elevator.empty()) 
+	{
+		node now = elevator.front();
+		elevator.pop();
+
+		int floor = now.floor;
+		int moves = now.howmany_move;
+
+		if (floor == G)
+		{
+			answer_howmany_move = moves;
+			break;
+		}
+
+		if (floor + U <= F && !elevator_info::bVisited[floor + U])
+			elevator_info::bVisited[floor + U] = true, elevator.push({ floor + U, moves + 1 });
+		if (floor - D > 0 && !elevator_info::bVisited[floor - D])
+			elevator_info::bVisited[floor - D] = true, elevator.push({ floor - D, moves + 1 });
+	}
+
+	if (answer_howmany_move == -1) std::cout << "use the stairs";
+	else std::cout << answer_howmany_move;
+
+	return 0;
+}
--- 10week/README.md
+++ 10week/README.md
@@ -5,6 +5,7 @@
 [전체 문제 보기](https://www.acmicpc.net/problem/5014)
 ### 풀이
 - [nmp9981의 풀이](https://blog.naver.com/tybnasgo/222606071766)
+- [JohnNaksaing의 풀이](./JohnNaksaing/boj5014.md)
 
 ## BOJ 2252 줄 세우기
 ### 문제
