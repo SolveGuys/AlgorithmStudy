fc5d4b751acbf17386ec4d9db1b96c43bfd7d416
johnnaksaing <johnnaksaing@daum.net>
2022-01-08 18:13:12 +0900

    add prog 81303.md

--- 11week/JohnNaksaing/prog81303.cpp
+++ 11week/JohnNaksaing/prog81303.cpp
@@ -1,4 +1,3 @@
-#pragma once
 #include <string>
 #include <vector>
 #include <stack>
@@ -6,106 +5,209 @@
 
 #include <iostream>
 
-// 프로그래머스에서는 전처리기로 처리해야 올바르게 인식합니다
-//constexpr bool __USING_STL__ = true;
-
 #define __USING_STL__ 1
 
-using table_ds = std::set<int>;
-using iterator = std::set<int>::iterator;
-
-using std::prev;
-using std::next;
-
-#if __USING_STL__
-std::string solution(int n, int k, std::vector<std::string> cmd) {
-    std::cout << "from __USING_STL__";
-    std::string answer(n,'X');
-    {
-        table_ds table;
-
-        for (int i = 0; i < n; i++)
-        {
-            table.insert(i);
-        }
-
-        iterator cursor = table.find(k);
-        std::stack<int> stack_history;
-        
-        for (std::string query : cmd)
-        {
-            char control = query[0];
-
-            switch (control)
-            {
-            case 'U':
-            case 'D':
-            {
-                int X = std::stoi(query.substr(2));
-
-                control == 'U' ? cursor = prev(cursor, X) : cursor = next(cursor, X);
-
-                //std::cout << *cursor << ',';
-                break;
-            }
-            case 'C':
-                stack_history.push(*cursor);
-                cursor = table.erase(cursor);
-                if(cursor == table.end()) cursor = prev(cursor);
-                break;
-
-            case 'Z':
-            {
-                int revert = stack_history.top();
-                stack_history.pop();
-                table.insert(revert);
-                break;
-            }
-            default:
-                return "oops";
-                break;
-            }
-        }
-
-        for (int i : table) /*std::cout << i << ' ',*/ answer[i] = 'O';
-
-
-    }
-    return answer;
-}
-
-//아래 구현 끝나면 else 제거할 수 있게 인터페이스 처리할 것
-#else
-
-class node
+struct node
 {
+	int num;
+	node* next;
+	node* prev;
+public:
+	node(int num) :num(num), next(nullptr), prev(nullptr) {}
 
+	node* operator*() { return this; }
 };
+
 class double_ll
 {
+	node* head;
+	node* tail;
+
 public:
-	node find(int k){ return node{};}
-    void insert(int val){}
-    
-    node begin() {return node{};}
-    node end() {return node{};}
+
+	node* find(int k);
+	void insert(int val);
+	void insert(node* node);
+	node* erase(node* target);
+
+	node* begin() { return head; }
+	node* end() { return tail; }
+
+	double_ll() : head(nullptr),tail(nullptr) {}
 };
 
-node prev(node& iter, const int& offset){ return iter; }
-node next(node& iter, const int& offset){ return iter; }
+node* double_ll::find(int k)
+{
+	node* temp = head;
+	do
+	{
+		if (temp->num == k) return temp;
+		else temp = temp->next;
+	} while (temp->next != nullptr);
+	return nullptr;
+}
 
+//insert when initializing
+void double_ll::insert(int val) 
+{
+	node* temp = new node(val);
 
-using table_ds = double_ll;
+	if (head == nullptr) head = temp;
+	if (tail != nullptr) 
+	{
+		temp->prev = tail;
+		tail->next = temp;
+	}
+	tail = temp;
+}
 
-table_iterator prev(table_iterator& iter, const int& offset);
-table_iterator next(table_iterator& iter, const int& offset);
+//revert
+void double_ll::insert(node* node) 
+{
+	if (node->prev == nullptr && node->next == nullptr) 
+	{
+		head = node;
+		tail = node;
+	}
+	else if (node->prev == nullptr) 
+	{
+		node->next = head;
+		head = node;
+	}
+	else if (node->next == nullptr) 
+	{
+		tail->next = node;
+		tail = node;
+	}	
+	else 
+	{
+		node->prev->next = node;
+		node->next->prev = node;
+	}
+}
+
+node* double_ll::erase(node* target) 
+{
+	if (target == head) 
+	{
+		if(target->next)
+			target->next->prev = nullptr;
+		head = target->next;
+	}
+	else if (target == tail) 
+	{
+		if(target->prev)
+			target->prev->next = nullptr;
+		tail = target->prev;
+		return tail;
+	}
+	else 
+	{
+		target->prev->next = target->next;
+		target->next->prev = target->prev;
+	}
+
+	return target->next;
+}
+
+node* prev(node* iter, int offset = 1)
+{
+	while (offset--) iter = iter->prev;
+	return iter;
+}
+
+node* next(node* iter, int offset = 1)
+{
+	while (offset--) iter = iter->next;
+	return iter;
+}
+
 
 std::string solution(int n, int k, std::vector<std::string> cmd) {
-    std::cout << "from __USING_DOUBLE_LL__";
-    std::string answer(n,'X');
 
-    //TODO : List를 이용하여 구현
+#if __USING_STL__
+	using table_ds = std::set<int>;
+	using iterator = std::set<int>::iterator;
+    using stack_ds = std::stack<int>;
+	using std::prev;
+	using std::next;
 
+	std::cout << "from __USING_STL__" << '\n';
+#else
+    using table_ds = double_ll;
+	using iterator = node*;
+	using stack_ds = std::stack<node*>;
+
+
+	std::cout << "from __USING_DOUBLEL_LL__\n";
+#endif
+    
+	std::string answer(n, 'X');
+	{
+		table_ds table;
+
+		for (int i = 0; i < n; i++)
+		{
+			table.insert(i);
+		}
+
+		iterator cursor = table.find(k);
+		stack_ds stack_history;
+
+		for (std::string query : cmd)
+		{
+			char control = query[0];
+
+			switch (control)
+			{
+			case 'U':
+			case 'D':
+			{
+				int X = std::stoi(query.substr(2));
+
+				control == 'U' ? cursor = prev(cursor, X) : cursor = next(cursor, X);
+
+				//std::cout << *cursor << ',';
+				break;
+			}
+			case 'C':
+            #if __USING_STL__        
+				stack_history.push(*cursor);
+            #else
+                stack_history.push(cursor);   
+            #endif
+				cursor = table.erase(cursor);
+            #if __USING_STL__
+				if (cursor == table.end()) cursor = prev(cursor);
+            #endif
+				break;
+
+			case 'Z':
+			{
+				auto revert = stack_history.top();
+				stack_history.pop();
+				table.insert(revert);
+				break;
+			}
+			default:
+				return "oops";
+				break;
+			}
+		}
+
+#if __USING_STL__
+		for (int i : table) /*std::cout << i << ' ',*/ answer[i] = 'O';
+#else
+		iterator temp = table.begin();
+		while (temp != nullptr) 
+		{
+			/*std::cout << i << ' ',*/ 
+			int i = temp->num;
+			answer[i] = 'O'; 
+			temp = temp->next;
+		}
+#endif
+
+	}
 	return answer;
 }
-#endif
