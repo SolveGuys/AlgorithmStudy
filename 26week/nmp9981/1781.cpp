aecdd8841e65c229d7baf3398896b1c54c74ab08
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2022-05-08 19:50:46 +0900

    Update 1781.cpp

--- 26week/nmp9981/1781.cpp
+++ 26week/nmp9981/1781.cpp
@@ -1,48 +1,47 @@
 #include <iostream>
-#include <queue>
-#include <vector>
 #include <algorithm>
+#include <queue>
 using namespace std;
 
 int n;
-vector<pair<int, int>> ramen;
-
+vector<pair<int, int>> ramen;//컵라면
 //입력
 void input() {
 	cin >> n;
 	for (int i = 0; i < n; i++) {
-		int date, reward;
-		cin >> date >> reward;
-		ramen.push_back({ date,reward });
+		int deadline, reward;
+		cin >> deadline >> reward;
+		ramen.push_back({ deadline,reward });
 	}
-	sort(ramen.begin(), ramen.end());//날짜 기준으로 정렬
+	sort(ramen.begin(), ramen.end());//데드라인 기준으로 정렬
 }
 
-//라면 먹기
+//먹기
 void eat() {
-	priority_queue<int,vector<int>,greater<int>> pq;//최소 힙
-	for (int i = 0; i < n; i++) {
-		int day = ramen[i].first;//마감 기한
-		int score = ramen[i].second;//보상
+	priority_queue<int, vector<int>, greater<int>> pq;//최소힙
+	for (int i = 0; i < ramen.size(); i++) {
+		int deadline = ramen[i].first;
+		int reward = ramen[i].second;
 
-		pq.push(score);//최소 힙에 넣기
+		pq.push(reward);
 		//용량초과
-		if (day < pq.size()) pq.pop();//가장 작은것 빼기
+		if (deadline < pq.size()) pq.pop();
 	}
-	//최대 보상 구하기
-	int sum = 0;
+	//힙에있는 원소들의 합
+	int total = 0;
 	while (!pq.empty()) {
-		sum += pq.top();
+		total += pq.top();
 		pq.pop();
 	}
-	cout << sum;//출력
+	cout << total;//출력
 }
+
 int main() {
 	//빠른 입력
 	ios_base::sync_with_stdio(false);
 	cin.tie(0);
 
 	input();//입력
-	eat();//라면 먹기
+	eat();//먹기
 	return 0;
 }
