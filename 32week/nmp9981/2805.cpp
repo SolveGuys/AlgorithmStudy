7194020c8642c30d733be228bbf96660318ff4ab
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2022-06-16 19:57:27 +0900

    Create 2805.cpp

---
+++ 32week/nmp9981/2805.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+
+#define maxi 1000001
+using namespace std;
+typedef long long ll;
+
+ll n;//나무의 수
+ll m;//나무의 길이
+ll tree_height[maxi];//나무의 높이
+
+//입력
+void input() {
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) cin >> tree_height[i];
+}
+//남은 나무의 길이
+ll rest_tree(ll x) {
+	ll rest = 0;
+	for (int i = 0; i < n; i++) rest += max(0ll, tree_height[i] - x);
+	return rest;
+}
+
+//나무자르기
+void cut_tree() {
+	ll min_height = 0;
+	ll max_height = 1000000000;
+	
+	while (min_height <= max_height) {
+		ll height = (min_height + max_height) / 2;//중앙값
+		if (rest_tree(height) >= m) {//높이 증가
+			min_height = height + 1;
+		}
+		else {
+			max_height = height - 1;
+		}
+	}
+	cout << max_height;//출력
+}
+int main() {
+	//빠른 입력
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	
+	input();//입력
+	cut_tree();//나무 자르기
+	return 0;
+}
