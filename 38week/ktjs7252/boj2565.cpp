e90c462883f47e460c126181809ccf57fabfeba4
ktjs7252 <78638160+ktjs7252@users.noreply.github.com>
2022-07-28 13:09:22 +0900

    Create boj2565.cpp

---
+++ 38week/ktjs7252/boj2565.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, len, num;
+pair<int, int> a[100001];
+int lis[100001];
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+	cin>>n;
+	for (int i = 0; i < n; i++){
+        cin>>a[i].first>>a[i].second;
+    }
+    sort(a, a + n);
+    for(int i = 0; i < n; i++){
+        auto it = lower_bound(lis, lis + len, a[i].second);
+        if(!(*it))len++;
+        *it = a[i].second;
+    }
+    cout<<n-len;
+
+	return 0;
+}
+
+
