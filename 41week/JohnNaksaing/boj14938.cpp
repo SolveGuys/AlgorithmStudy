e02222c7761db1e0eaad99571332ada622bf56cf
johnnaksaing <johnnaksaing@daum.net>
2022-08-27 21:16:16 +0900

    Update boj14938.cpp

--- 41week/JohnNaksaing/boj14938.cpp
+++ 41week/JohnNaksaing/boj14938.cpp
@@ -27,14 +27,14 @@ int main()
 	}
 
 	for (int k = 1; k <= n; k++)
-    for (int i = 1; i <= n; i++)
-    for (int j = 1; j <= n; j++)
-        if (map[i][j] > map[i][k] + map[k][j])
-            map[i][j] = map[i][k] + map[k][j];
+    	for (int i = 1; i <= n; i++)
+    	for (int j = 1; j <= n; j++)
+	    if (map[i][j] > map[i][k] + map[k][j])
+            	map[i][j] = map[i][k] + map[k][j];
 
 	int result = 0;
 	for (int i = 1; i <= n; i++) 
-    {
+    	{
 		int temp = 0;
 		for (int j = 1; j <= n; j++)
 			if (map[i][j] <= m)
