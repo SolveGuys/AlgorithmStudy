5df0e2718ea0aa4bd85b3f5556cc70a7317d1366
ktjs7252 <78638160+ktjs7252@users.noreply.github.com>
2022-09-23 13:15:49 +0900

    Update poj72411x.cpp

--- 44week/ktjs7252/poj72411x.cpp
+++ 44week/ktjs7252/poj72411x.cpp
@@ -1,4 +1,3 @@
-```c++
 #include <bits/stdc++.h>
 #include <string>
 #include <vector>
@@ -91,4 +90,84 @@ vector<string> solution(vector<string> orders, vector<int> course) {
     
     return answer;
 }
-```
+
+//
+#include <bits/stdc++.h>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+map<string,int> m; //무작위 배열, 몇개인지
+string arr;
+int num[12]; //몇개가 최대인지
+
+void solve(vector<string> &orders,int idx,string tmp)
+{
+    if(idx==arr.size())
+    {
+        int counting=0;
+        for(int i=0;i<orders.size();i++)
+        {
+            int x=0;
+            for(int j=0;j<tmp.size();j++)
+            {
+                if(orders[i].find(tmp[j])!=string::npos)
+                {
+                    x++;
+                }
+            }
+            if(x==tmp.size())
+            {
+                counting++;
+            }
+            
+        }
+        
+        m[tmp]=counting;
+        num[tmp.size()]=max(num[tmp.size()],counting);
+        
+        return;
+    }
+    
+    solve(orders,idx+1,tmp+arr[idx]);
+    solve(orders,idx+1,tmp);
+    
+}
+
+vector<string> solution(vector<string> orders, vector<int> course) {
+    vector<string> answer;
+    
+    for(int i=0;i<orders.size();i++)
+    {
+        for(int j=0;j<orders[i].size();j++)
+        {
+            if(arr.find(orders[i][j])==string::npos)
+            {
+                arr+=orders[i][j];
+            }
+        }
+    }
+    sort(arr.begin(),arr.end());
+    
+    solve(orders,0,"");
+    
+    for(int i=0;i<course.size();i++)
+    {
+        for(auto x:m)
+        {
+            //cout<<x.first<<" "<<x.second<<"\n";
+            if(num[course[i]]==x.second && course[i]==x.first.size() && x.second>=2)
+            {
+                //cout<<x.first<<" "<<x.second<<" "<<num[course[i]]<<" "<<course[i]<<"\n";
+                answer.push_back(x.first);
+            }
+            
+        }
+    }
+    sort(answer.begin(),answer.end());
+    
+    return answer;
+}
+//
+
