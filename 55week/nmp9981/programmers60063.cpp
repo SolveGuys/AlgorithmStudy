51108d6159693ae627310f939287e619f0d3bb43
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2022-12-15 17:25:45 +0900

    Update programmers60063.cpp

--- 55week/nmp9981/programmers60063.cpp
+++ 55week/nmp9981/programmers60063.cpp
@@ -1,86 +1,118 @@
-#include <iostream>
-#include <cstring>
+#include <string>
 #include <vector>
 #include <queue>
 using namespace std;
 
-struct Pos {
-    int xpos, ypos, direction, cnt;
+struct Robot {//로봇 상태
+    int time;//시간
+    int x, y;//위치
+    int type;//가로, 세로
 };
-
 int n;//board크기
 const int maxi = 101;
-bool visit[maxi][maxi][4];//방문 시간
-vector<vector<int>> board = { {0,0,0,1,1},{0,0,0,1,0},{0,1,0,1,1},{1,1,0,0,1},{0,0,0,0,0} };
+bool visit[maxi][maxi][2];//방문 여부(위치, 모양)
+int dx[4] = { 1,-1,0,0 }; int dy[4] = { 0,0,1,-1 };//이동
 
-int dx[4] = { 0,-1,0,1 }; int dy[4] = { -1,0,1,0 };//탐색
-
-//탐색 가능한가?
-bool isNotCan(int nx, int ny, vector<vector<int>> board) {
-    if (nx < 0 || nx >= n || ny < 0 || ny >= n) return true;//범위체크
-    if (board[nx][ny] == 1) return true;//장애물
-    return false;
+//체크
+bool isPossible(int x,int y, int type, vector<vector<int>> board) {
+    if (type == 0) {//가로
+        if (x < 0 || x >= n || y < 0 || y + 1 >= n) return false;//범위 조건
+        if (board[x][y] == 1 || board[x][y + 1] == 1) return false;//장애물
+    }
+    else if (type == 1) {//세로
+        if (x < 0 || x+1 >= n || y < 0 || y >= n) return false;//범위 조건
+        if (board[x+1][y] == 1 || board[x][y] == 1) return false;//장애물
+    }
+    return true;
 }
+
 //로봇 이동
 int Move(vector<vector<int>> board) {
-    n = board.size();
-    memset(visit, false, sizeof(visit));//초기화
-    queue<Pos> q;//로봇의 앞쪽 정보
-    //앞쪽을 넣기
-    q.push({ 0,1,0,0 });//위치, 방향, 시간
-    visit[0][1][0] = true;
-    q.push({ 0,0,2,0 });
-    visit[0][0][2] = true;
+    n = board.size();//지도 크기
+    visit[0][0][0] = true;//초기 지점 방문 체크(x,y가 더 작은쪽)
+    queue<Robot> q;//로봇 위치
+    q.push({ 0,0,0,0 });
 
     while (!q.empty()) {
-        //현재 로봇 정보
-        int x1 = q.front().xpos; int y1 = q.front().ypos;//위치
-        int dir = q.front().direction;//방향
-        int sec = q.front().cnt;//시간
+        Robot now = q.front();//로봇 위치
         q.pop();
-        int x2 = x1 + dx[dir]; int y2 = y1 + dy[dir];//로봇 뒤쪽 위치
+       
         //방문 완료
-        if ((x1 == n - 1 && y1 == n - 1) || (x2 == n - 1 && y2 == n - 1)) return sec;
+        if ((now.type==0 && now.x==n-1 && now.y==n-2) || (now.type==1 && now.x==n-2 && now.y==n-1)) {
+            return now.time;
+        }
 
-        //다음 지점
+        //회전 X
         for (int i = 0; i < 4; i++) {
-            int nx1 = x1 + dx[i]; int ny1 = y1 + dy[i];
-            int nx2 = x2 + dx[i]; int ny2 = y2 + dy[i];
+            int nx = now.x + dx[i]; int ny = now.y + dy[i];
 
-            //탐색 가능 지점인가?
-            if (isNotCan(nx1, ny1, board) || isNotCan(nx2, ny2, board)) continue;
+            if (isPossible(nx, ny, now.type ,board) == false) continue;//탐색 가능한가?
+            if (visit[nx][ny][now.type] == true) continue;//방문 여부
 
-            //방문여부
-            if (visit[nx1][ny1][dir] == false) {
-                visit[nx1][ny1][dir] = true;
-                q.push({ nx1,ny1,dir,sec + 1 });
-            }
-            if (visit[nx2][ny2][(dir + 2) % 4] == false) {//앞면이 바뀜
-                visit[nx2][ny2][(dir + 2) % 4] = true;
-                q.push({ nx2,ny2,(dir + 2) % 4,sec + 1 });
-            }
-            //회전 가능할 때
-            if ((dir + 1) % 2 == i % 2) {
-                if (visit[nx1][ny1][i] == false) {
-                    visit[nx1][ny1][i] = true;
-                    q.push({ nx1,ny1,i,sec + 1 });
+            visit[nx][ny][now.type] = true;
+            q.push({ now.time+1,nx,ny,now.type });
+        }
+        //회전
+        if (now.type == 0) {//현재 가로
+            //왼쪽이 축
+            //위로 회전
+            if (now.x - 1 >= 0 && board[now.x - 1][now.y] == 0 && board[now.x - 1][now.y + 1] == 0) {//범위, 장애물
+                if (visit[now.x - 1][now.y][1] == false) {//방문 조건
+                    q.push({ now.time + 1,now.x - 1,now.y,1 });
                 }
-                if (visit[nx2][ny2][(i + 2) % 4] == false) {//앞면 변경
-                    visit[nx2][ny2][(i + 2) % 4] = true;
-                    q.push({ nx2,ny2,(i + 2) % 4,sec + 1 });
+            }
+            //아래로 회전
+            if (now.x + 1 < n && board[now.x + 1][now.y] == 0 && board[now.x + 1][now.y + 1] == 0) {//범위, 장애물
+                if (visit[now.x][now.y][1] == false) {//방문 조건
+                    q.push({ now.time + 1,now.x ,now.y,1 });
+                }
+            }
+            //오른쪽이 축
+            //위로 회전
+            if (now.x - 1 >= 0 && board[now.x - 1][now.y] == 0 && board[now.x - 1][now.y+1] == 0) {//범위, 장애물
+                if (visit[now.x - 1][now.y+1][1] == false) {//방문 조건
+                    q.push({ now.time + 1,now.x - 1,now.y+1,1 });
+                }
+            }
+            //아래로 회전
+            if (now.x + 1 < n && board[now.x + 1][now.y] == 0 && board[now.x + 1][now.y + 1] == 0) {//범위, 장애물
+                if (visit[now.x][now.y+1][1] == false) {//방문 조건
+                    q.push({ now.time + 1,now.x ,now.y+1,1 });
+                }
+            }
+        }
+        else if (now.type == 1) {//현재 세로
+            //위쪽이 축
+            //왼쪽으로 회전
+            if (now.y - 1 >= 0 && board[now.x][now.y-1] == 0 && board[now.x + 1][now.y - 1] == 0) {//범위, 장애물
+                if (visit[now.x][now.y-1][0] == false) {//방문 조건
+                    q.push({ now.time + 1,now.x,now.y-1,0 });
+                }
+            }
+            //오른쪽으로 회전
+            if (now.y + 1 < n && board[now.x][now.y+1] == 0 && board[now.x + 1][now.y + 1] == 0) {//범위, 장애물
+                if (visit[now.x][now.y][0] == false) {//방문 조건
+                    q.push({ now.time + 1,now.x ,now.y,0 });
+                }
+            }
+            //아래쪽이 축
+            //왼쪽으로 회전
+            if (now.y - 1 >= 0 && board[now.x + 1][now.y - 1] == 0 && board[now.x][now.y-1] == 0) {//범위, 장애물
+                if (visit[now.x + 1][now.y-1][0] == false) {//방문 조건
+                    q.push({ now.time + 1,now.x + 1,now.y-1,0 });
+                }
+            }
+            //오른쪽으로 회전
+            if (now.y + 1 < n && board[now.x][now.y+1] == 0 && board[now.x + 1][now.y + 1] == 0) {//범위, 장애물
+                if (visit[now.x+1][now.y][0] == false) {//방문 조건
+                    q.push({ now.time + 1,now.x+1 ,now.y,0 });
                 }
             }
         }
     }
     return 0;
 }
-
-int main() {
-	//빠른 입력
-	ios_base::sync_with_stdio(false);
-	cin.tie(0);
-
+int solution(vector<vector<int>> board) {
     int answer = Move(board);
-    cout << answer<<"\n";
-	return 0;
+    return answer;
 }
