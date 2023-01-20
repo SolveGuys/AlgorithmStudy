392156e98cc0b6d0a2a787b42bf8025dc3a96fd0
nmp9981 <84570138+nmp9981@users.noreply.github.com>
2023-01-20 09:50:03 +0900

    Update 16500.cpp

--- 58week/nmp9981/16500.cpp
+++ 58week/nmp9981/16500.cpp
@@ -1,34 +1,33 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <cstring>
 using namespace std;
 
-string goal;//목표 문자
 int n;
+string goal;//목표 문자열
 const int maxi = 101;
 string word[maxi];//단어 목록
-int cache[maxi];//i번부터 끝까지 완성 되었는가?
+bool isMake[maxi];//i~goal.size()번까지 만들 수 있는가?
 
 //입력
 void input() {
-    cin >>goal >> n;
+    cin >> goal;
+    cin >> n;
     for (int i = 0; i < n; i++) cin >> word[i];
 }
-//문자열 판별
-void Judge() {
-    cache[goal.size()] = 1;//초기값
-    for (int i = goal.size() - 1; i >= 0; i--) {
-        for (int j = 0; j < n; j++) {
-            if (goal.find(word[j], i) == i && cache[i + word[j].length()] == 1) {
-                cache[i] = 1;
-                break;
+//문자열 만들기
+void MakeString() {
+    isMake[goal.size()] = true;
+    for (int i = goal.size()-1; i >= 0; i--) {
+        for (int j = 0; j < n; j++) {//단어를 쭉 보면서
+            //이어서 붙일 수 있고 부분 단어가 일치하는가
+            if (isMake[i+word[j].size()]==true && goal.substr(i,word[j].size())==word[j]) {//합칠 수 있는가?
+                isMake[i] = true;
+                break;//단어가 이어졌으므로 해당 반복문은 빠져나간다.
             }
-            else {
-                cache[i] = 0;
-            }
+            else isMake[i] = false;
         }
     }
-    cout << cache[0];
+    cout << isMake[0];//0~goal.size() => 목표 문자열을 만들 수 있는가?
 }
 int main() {
     //빠른 입력
@@ -36,6 +35,6 @@ int main() {
     cin.tie(0);
 
     input();//입력
-    Judge();//문자열 판별
+    MakeString();//문자열 만들기
     return 0;
 }
