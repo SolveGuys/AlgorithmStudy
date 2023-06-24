d9f471fd1cbe03d7a92e2a5712f0e87b9a0b6e4a
HAEUL99 <71214577+HAEUL99@users.noreply.github.com>
2023-06-25 08:00:00 +0900

    Create 1600.cpp

---
+++ 72week/HAEUL99/1600.cpp
@@ -0,0 +1,94 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <tuple>
+#include <set>
+#include <cstring>
+#include <stdio.h>
+using namespace std;
+
+
+int board[202][202];
+int dx[4] = {0, 0, -1, 1};
+int dy[4] = {-1, 1, 0, 0};
+
+int dxx[8] = {2, 1, -2, -1, -2, -1, 2, 1};
+int dyy[8] = {1, 2, 1, 2, -1, -2, -1, -2};
+
+int visited[202][202][32]; 
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int k, n, m;
+    cin >> k;
+    cin >> m >> n; 
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < m; j++)
+        {
+            cin >> board[i][j];
+        }
+    }
+
+    fill(&visited[0][0][0], &visited[201][201][31], -1);
+    bool isTrue = false;
+    queue<tuple<int, int, int> > _queue;
+    _queue.push(make_tuple(0, 0, 0));
+   visited[0][0][0] = 1;
+
+    while(!_queue.empty())
+    {
+        int x, y, horse;
+        tie(x, y, horse) = _queue.front();
+        _queue.pop();
+
+        if(x == n -1 && y == m - 1)
+        {
+            cout << --visited[x][y][horse];
+
+            isTrue = true;
+            return 0;
+        }
+
+        for(int dir = 0; dir < 4; dir++)
+        {
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
+
+            if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+            if(visited[nx][ny][horse] == -1 && board[nx][ny] == 0)
+            {
+                _queue.push(make_tuple(nx, ny, horse));
+                visited[nx][ny][horse] = visited[x][y][horse] + 1;
+            }
+        }
+
+
+        if(horse + 1 <= k)
+        {
+            for(int dir = 0; dir < 8; dir++)
+            {
+                int nx = x + dxx[dir];
+                int ny = y + dyy[dir];
+
+                if(nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+                if(visited[nx][ny][horse + 1] == -1 && board[nx][ny] == 0)
+                {
+                    _queue.push(make_tuple(nx, ny, horse + 1));
+                    visited[nx][ny][horse + 1] = visited[x][y][horse] + 1;
+                }
+            }
+
+        }
+        
+    }
+    if(!isTrue)
+        cout << "-1";
+    
+    
+}
