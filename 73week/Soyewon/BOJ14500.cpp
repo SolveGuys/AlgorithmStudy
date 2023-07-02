23124e883d6ed013f25e8505545d8eda36d1445b
Yewon So <86148470+Soyewon@users.noreply.github.com>
2023-07-02 16:12:25 +0900

    Update BOJ14500.cpp

--- 73week/Soyewon/BOJ14500.cpp
+++ 73week/Soyewon/BOJ14500.cpp
@@ -1,85 +1,74 @@
 // 백준 14500 - 테트로미노
 
-#include <iostream>
-#include <algorithm>
+#include<iostream>
+#include<algorithm>
+#include<cstring>
 
 using namespace std;
 
-// 상하좌우 이동
-int dx[4] = {0, 0, -1, 1}; 
-int dy[4] = {1, -1, 0, 0};
 
-int map[500][500];
-int visit[500][500];
-int n, m, result; // 가로, 세로, 결과
+int map[501][501];
+int visit[501][501];
 
-// ㅏ모양 체크
-void FindOtherShaperesult(int y, int x)
-{
-    // ㅏ
-    if (y + 2 < n && x + 1 < m)
-        result = max(result, map[y][x] + map[y + 1][x] + map[y + 1][x + 1] + map[y + 2][x]);
-    // ㅜ
-    if (y + 1 < n && x + 2 < m)
-        result = max(result, map[y][x] + map[y][x + 1] + map[y][x + 2] + map[y + 1][x + 1]);
-    // ㅓ
-    if (y + 2 < n && x - 1 >= 0)
-        result = max(result, map[y][x] + map[y + 1][x] + map[y + 1][x - 1] + map[y + 2][x]);
-    // ㅗ
-    if (y - 1 >= 0 && x + 2 < m)
-        result = max(result, map[y][x] + map[y][x + 1] + map[y][x + 2] + map[y - 1][x + 1]);
-}
+int dx[] = {-1, 0, 0, 1};
+int dy[] = {0, -1, 1, 0};
 
-// depth 4인 도형들 체크
-void FindDepth4Mac(int L, int sum, int y, int x)
-{
-    if (L == 4)
-    {
-        result = max(result, sum);
+int N, M, res=0;
+
+void GetMaxNum(int x, int y, int depth, int temp) {
+
+    if(depth == 4) { // 깊이가 4가 되면
+        if(res < temp) res = temp;
         return;
     }
-    for (int i = 0; i < 4; i++)
-    {
-        int nx = x + dx[i]; // 상하좌우 이동
+
+    for(int i=0; i<4; i++) {
+        int nx = x + dx[i];
         int ny = y + dy[i];
-        if (ny < 0 || nx < 0 || ny >= N || nx >= M)
+        if(nx < 1 || ny < 1 || nx > N || ny > M || visit[nx][ny]) 
             continue;
-        if (visit[ny][nx])
-            continue;
-        visit[ny][nx] = true; 
-        findDepth4Max(L + 1, sum + map[ny][nx], ny, nx);
-        visit[ny][nx] = false; // 새로 dfs 돌기 전 visit 초기화
+
+        visit[nx][ny] = 1;
+        GetMaxNum(nx, ny, depth+1, temp+map[nx][ny]);
+        visit[nx][ny] = 0; // 백트래킹
     }
-}
 
-void FindMax(int y, int x)
-{
-    FindOtherShapeMax(y, x); // depth가 4가 아닌 'ㅏ' 먼저 찾기
+    // ㅓ
+    if(x-1 >= 1 && y-1 >= 1 && x+1 <= N) {
+        res = max(res, (map[x-1][y] + map[x][y-1] + map[x][y] + map[x+1][y]));
+    }
+    // ㅏ
+    if(x-1 >= 1 && y+1 <= M && x+1 <= N) { 
+        res = max(res, (map[x-1][y] + map[x][y+1] + map[x][y] + map[x+1][y]));
+    }
+    // ㅗ
+    if(y-1 >= 1 && y+1 <= M && x+1 <= N) { 
+        res = max(res, (map[x][y] + map[x+1][y] + map[x+1][y-1] + map[x+1][y+1]));
+    }
+    // ㅜ
+    if(y-1 >= 1 && y+1 <= M && x+1 <= N) { 
+        res = max(res, (map[x][y-1] + map[x][y] + map[x][y+1] + map[x+1][y]));
+    }
 
-    visit[y][x] = true;
-    FindDepth4Max(1, map[y][x], y, x);
-    visit[y][x] = false;
 }
 
 int main() {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
 
-    cin >> n >> m;
+    memset(visit, 0, sizeof(visit));
 
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<m; j++) {
+    cin >> N >> M;
+    for(int i=1; i<=N; i++) { 
+        for(int j=1; j<=M; j++) {
             cin >> map[i][j];
         }
     }
 
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<m; j++) {
-            FindMax(i, j);
+    for(int i=1; i<=N; i++) {
+        for(int j=1; j<=M; j++) {
+            visit[i][j] = 1;
+            GetMaxNum(i, j, 1, map[i][j]);
+            visit[i][j] = 0; // 백트래킹
         }
     }
-
-    cout << result;
-    return 0;
-
+    cout << res;
 }
