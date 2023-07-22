9ebf9be38b01f33a42e43594c6de58ac1a4c883c
HAEUL99 <71214577+HAEUL99@users.noreply.github.com>
2023-07-23 01:08:35 +0900

    Create 22862.cpp

---
+++ 76week/HAEUL99/22862.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, k;
+int arr[1000002];
+int res[1000002];
+int main()
+{           
+    cin >> n >> k;
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int ok = k;
+    int en = 0;
+    
+    if(arr[0] % 2 != 0)
+    {
+        k--;
+    }
+    
+    for(int st = 0; st < n; st++)
+    {
+        while(en < n - 1)
+        {
+            en++;
+            if(arr[en] % 2 != 0)
+            {
+                if(k > 0)
+                    k--;
+                else
+                {
+                    en--;
+                    break;
+                }
+            }
+        }
+        res[st] = en - st - (ok - k) + 1;
+
+        if(arr[st] % 2 != 0)
+        {
+            k++;
+        }
+        
+    }
+
+    cout << *max_element(res, res+n);
+}
