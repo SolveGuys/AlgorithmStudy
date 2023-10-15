a9045c6c76353d956b14f28ac76ab824082de7d6
HAEUL99 <71214577+HAEUL99@users.noreply.github.com>
2023-10-15 18:25:42 +0900

    Update 13460.cpp

--- 87week/HAEUL99/13460.cpp
+++ 87week/HAEUL99/13460.cpp
@@ -1,198 +1,164 @@
-#include<bits/stdc++.h>
+#include<iostream>
+#include<vector>
+#include <climits>
 using namespace std;
 
-string board[12];
-int hx, hy;
 int n, m;
+int hx, hy;
+string board[12];
 int dx[4] = {0, 0, -1, 1};
 int dy[4] = {-1, 1, 0, 0};
-bool isFind = false;
 int ans = INT_MAX;
-void dfs(int cur,int bx, int by, int rx, int ry)
+void moveR(int dir, int& rx, int& ry)
 {
-    if(cur > 10)
+    while(true)
     {
-        return;
-    }
-    
-    //성공 (빨간 구슬만 빠짐)
-    if((rx == hx && ry == hy) && !(bx == hx && by == hy)) 
-    {
-        ans = min(ans, cur);
-        return;
-    }
+        int nx = rx + dx[dir];
+        int ny = ry + dy[dir];
 
-    //실패 (둘다 빠짐)
-    if((rx == hx && ry == hy) && (bx == hx && by == hy)) 
-    {
-        return;
-    }
-
-    //실패 (파란 구슬만 빠짐)
-    if(!(rx == hx && ry == hy) && (bx == hx && by == hy)) 
-    {
-        return;
-    }
-    int origin_rx = rx;
-    int origin_ry = ry;
-    int origin_bx = bx;
-    int origin_by = by;
-    string origin_board[12];
-    for(int i = 0; i < n; i++)
-    {
-        for(int j = 0; j < m; j++)
+        if(board[nx][ny] == '#') break;
+        if(board[nx][ny] == 'B') break;
+        if(board[nx][ny] == 'O')
         {
-            origin_board[i][j] = board[i][j];
+            board[rx][ry] = '.';
+            rx = nx;
+            ry = ny;
+            break;
+        }
+        if(board[nx][ny] == '.')
+        {
+            board[nx][ny] = 'R';
+            board[rx][ry] = '.';
+            rx = nx;
+            ry = ny;   
         }
     }
-    for(int dir = 0; dir < 4; dir++)
+}
+
+void moveB(int dir, int& bx, int& by)
+{
+    while(true)
     {
-        int first = 0; // 0: r, 1: b
-        
-        rx = origin_rx; ry = origin_ry; bx = origin_bx; by = origin_by;
+        int nx = bx + dx[dir];
+        int ny = by + dy[dir];
 
-        for(int i = 0; i < n; i++)
+        if(board[nx][ny] == '#') break;
+        if(board[nx][ny] == 'R') break;
+        if(board[nx][ny] == 'O')
         {
-            for(int j = 0; j < m; j++)
-            {
-                board[i][j] = origin_board[i][j];
-            }
+            board[bx][by] = '.';
+            bx = nx;
+            by = ny;
+            break;
         }
+        if(board[nx][ny] == '.')
+        {
+            board[nx][ny] = 'B';
+            board[bx][by] = '.';
+            bx = nx;
+            by = ny;
+        }
+    }
+}
 
+void dfs(int cur, int bx, int by, int rx, int ry)
+{
+    if(cur > 10) return;
+    if(bx == hx && by == hy) return;
+    if(rx == hx && ry == hy)
+    {
+        ans = min(cur, ans);
+        return;
+    }
+    int curbx = bx; int curby = by;
+    int currx = rx; int curry = ry;
+    string curboard[12];
 
-        
+    for(int i = 0; i < n; i++) curboard[i] = board[i];
+    bool left = false; bool up = false; //빨강색이 더 왼쪽/ 위쪽 있는 경우 true
+    if(ry < by) left = true;
+    if(rx < bx) up = true;
+
+    for(int dir = 0; dir < 4; dir++) //왼 오 위 아래
+    {
+        bx = curbx; by = curby; rx = currx; ry = curry;
+        for(int i = 0; i < n; i++) board[i] = curboard[i];
         if(dir == 0)
         {
-            if(ry < by) first = 0;
-            else first = 1; 
+            if(left)
+            {
+                //r 먼저
+                moveR(dir, rx, ry);
+                moveB(dir, bx, by);
+            }
+            else
+            {
+                // b 먼저
+                moveB(dir, bx, by);
+                moveR(dir, rx, ry);
+            }
         }
+
         if(dir == 1)
         {
-            if(ry < by) first = 1;
-            else first = 0;
+            if(left)
+            {
+                //b 먼저
+                moveB(dir, bx, by);
+                moveR(dir, rx, ry);
+            }
+            else
+            {
+                // r 먼저
+                moveR(dir, rx, ry);
+                moveB(dir, bx, by);
+            }
         }
+
         if(dir == 2)
         {
-            if(rx < bx) first = 0;
-            else first = 1;
+            if(up)
+            {
+                //r 먼저
+                moveR(dir, rx, ry);
+                moveB(dir, bx, by);
+            }
+            else
+            {
+                // b 먼저
+                moveB(dir, bx, by);
+                moveR(dir, rx, ry);
+            }
         }
+
         if(dir == 3)
         {
-            if(rx < bx) first = 1;
-            else first = 0;
-        }
-
-       
-        if(first == 0)
-        {
-            //R 옮기기
-            while(true)
+            if(up)
             {
-                int nx = rx + dx[dir];
-                int ny = ry + dy[dir]; 
-
-                if(nx < 0 || nx > n-1 || ny < 0 || ny > m-1) break;
-                if(board[nx][ny] == '#') break;
-                if(nx == hx && ny == hy)
-                {
-                    board[nx][ny] = 'R'; board[rx][ry] = '.';
-                    rx = nx;
-                    ry = ny;
-   
-                    break;
-                }
-                board[nx][ny] = 'R'; board[rx][ry] = '.';
-                rx = nx;
-                ry = ny;
+                //b 먼저
+                moveB(dir, bx, by);
+                moveR(dir, rx, ry);
             }
-
-            //B 옮기기
-            while(true)
+            else
             {
-                int nx = bx + dx[dir];
-                int ny = by + dy[dir]; 
-
-                if(nx < 0 || nx > n-1 || ny < 0 || ny > m-1) break;
-                if(nx == hx && ny == hy)
-                {
-                    board[nx][ny] = 'B'; board[bx][by] = '.';
-                    bx = nx;
-                    by = ny;
-                    break;
-                }
-                if(board[nx][ny] == '#' || board[nx][ny] == 'R') break;
-                
-                board[nx][ny] = 'B'; board[bx][by] = '.';
-                bx = nx;
-                by = ny;
+                // r 먼저
+                moveR(dir, rx, ry);
+                moveB(dir, bx, by);
             }
         }
-        if(first == 1)
-        {
-            //B 옮기기
-            while(true)
-            {
-                int nx = bx + dx[dir];
-                int ny = by + dy[dir]; 
-
-                if(nx < 0 || nx > n-1 || ny < 0 || ny > m-1) break;
-                if(board[nx][ny] == '#') break;
-                if(nx == hx && ny == hy)
-                {
-                    board[nx][ny] = 'B'; board[bx][by] = '.';
-                    bx = nx;
-                    by = ny;
-                    break;
-                }
-                board[nx][ny] = 'B'; board[bx][by] = '.';
-                
-                bx = nx;
-                by = ny;
-            }
-
-            //R 옮기기
-            while(true)
-            {
-                int nx = rx + dx[dir];
-                int ny = ry + dy[dir]; 
-            
-                if(nx < 0 || nx > n-1 || ny < 0 || ny > m-1) break;
-                if(nx == hx && ny == hy)
-                {
-                    board[nx][ny] = 'R'; board[rx][ry] = '.';
-                    rx = nx;
-                    ry = ny;
-   
-                    break;
-                }
-                if(board[nx][ny] == '#' || board[nx][ny] == 'B') break;
-                
-                board[nx][ny] = 'R'; board[rx][ry] = '.';
-                rx = nx;
-                ry = ny;
-            }
-
-            
-        }
-
         
-
         dfs(cur+1, bx, by, rx, ry);
-
-
     }
-
-
 }
 
 int main ()
 {
-    
     cin >> n >> m;
 
     for(int i = 0; i < n; i++)
     {
         cin >> board[i];
+        
     }
     int bx, by, rx, ry;
     for(int i = 0; i < n; i++)
@@ -211,16 +177,11 @@ int main ()
             {
                 hx = i; hy = j;
             }
-
         }
     }
 
-
     dfs(0, bx, by, rx, ry);
-    if(ans == INT_MAX) ans = -1;
-    cout << ans;
-    
-
+    if(ans != INT_MAX)cout << ans;
+    else cout << "-1";
 
 }
-
