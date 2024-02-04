007ed92d5077c58cc3979201f6f7a2346a318594
Seunggu Kang <63611804+kang9366@users.noreply.github.com>
2024-02-04 21:19:06 +0900

    Update 2002.cpp

--- 103week/kang9366/2002.cpp
+++ 103week/kang9366/2002.cpp
@@ -17,11 +17,13 @@ int main() {
     queue<string> dq2;
     cin >> N;
 
+    // 들어오는 자동차 번호를 dequeue에 저장
     for(int i = 0; i < N; i++) {
         cin >> str;
         dq1.push_back(str);
     }
 
+    // 나오는 자동차 번호를 queue에 저장
     for(int i = 0; i < N; i++) {
         cin >> str;
         dq2.push(str);
@@ -29,15 +31,19 @@ int main() {
 
     int answer = 0;
 
+    // deque와 queue가 빌때까지 반복
     while(!dq1.empty() && !dq2.empty()) {
         if(dq1.front() != dq2.front()) {
+            // deque의 첫번째 원소와 queue의 첫번쨰 원소가 다른 경우
             answer++;
             dq1.erase(remove(dq1.begin(), dq1.end(), dq2.front()));
             dq2.pop();
         } else {
+            // deque의 첫번째 원소와 queue의 첫번쨰 원소가 다른 경우
             dq1.erase(dq1.begin());
             dq2.pop();
         }
     }
+    // 정답 출력
     cout << answer; 
 }
