8a3ba9332b65f1651f0a9d7fbd750582275c86c9
Choiwongang <choiwongang@gmail.com>
2024-03-24 21:40:39 +0900

    Update dragonCurve.cpp

--- 110week/winterobust/dragonCurve.cpp
+++ 110week/winterobust/dragonCurve.cpp
@@ -5,24 +5,29 @@ int N;
 bool grid[101][101];
 int dx[4] = {1, 0, -1, 0};  
 int dy[4] = {0, -1, 0, 1};
-
+//동,북,서,남
 int turn(int d) {
+    //90도 회전을 위해서
     return (d + 1) % 4;
 }
 
 void draw_curve(int x, int y, int d, int g) {
+    //세대 10개 90 도 회전 -> 2배씩 방향이 증가 2^10 = 1024
     int directions[1024] = {d}; 
+    //count 현재까지의 방향의 수를 의미
     int count = 1; 
-
+    
     for (int i = 0; i < g; i++) {
+        //현재 세대의 방향의 총수를 저장
         int prevCount = count;
+        //역수로 for문을 돔 
         for (int j = prevCount - 1; j >= 0; j--) {
             directions[count++] = turn(directions[j]);
         }
     }
     
     grid[y][x] = true;
-
+    //실제 이동 구현 mapping
     for (int i = 0; i < count; i++) {
         x += dx[directions[i]];
         y += dy[directions[i]];
@@ -39,6 +44,7 @@ int main() {
     }
 
     int result = 0;
+    // 정사각형 갯수 세기
     for (int i = 0; i < 101 - 1; i++) {
         for (int j = 0; j < 101 - 1; j++) {
             if (grid[i][j] && grid[i+1][j] && grid[i][j+1] && grid[i+1][j+1]) {
