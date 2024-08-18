9cbfc5b36eadbe2934c003ddad9d4d3d872e2758
kknyapple <72698829+kknyapple@users.noreply.github.com>
2024-08-18 22:58:24 +0900

    Update 4384.cpp

--- 129week/kknyapple/4384.cpp
+++ 129week/kknyapple/4384.cpp
@@ -24,7 +24,7 @@ int main() {
     // i명의 사람을 선택해 j 무게를 만들 수 있는지
     d[0][0] = true;
     for (int i = 0; i < n; i++) {
-        for (int j = n / 2; j > 0; j--) {
+        for (int j = n / 2; j > 0; j--) { // 인원수 내림차순으로
             for (int k = total; k >= a[i]; k--) {
                 d[j][k] = d[j][k] || d[j - 1][k - a[i]];
             }
@@ -32,11 +32,14 @@ int main() {
     }
 
     for (int i = 0; i <= total; i++) {
-        if (d[n / 2][i] == true) {
+        // 두 팀 간의 사람 수 차이가 1이하, i 무게 만들 수 있을 때 차이 계산
+        if (d[n / 2][i] == true) { 
+            // 두 팀의 몸무게
             int a = i;
             int b = total - i;
-            if (a < b) swap(a, b);
+            if (a < b) swap(a, b); // a >=b 가 되도록 만들어줌
 
+            // 두 팀간의 몸무게 차이가 최소화되도록 함
             if (diff > a - b) {
                 diff = a - b;
                 res1 = b;
